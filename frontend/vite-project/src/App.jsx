7dba8af03a20422484e7ce0e32ee7d0d7d0e7d8c
ankush adhikari <ankushadhikari321@gmail.com>
2025-06-21 23:41:01 +0545

    login and signup done to prompt page

--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -3,18 +3,20 @@ import './App.css'
 import Home from './components/Home'
 import Login from './components/login'
 import Signup from './components/signup'
-
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
 
   return (
-    
+    <>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login/>} /> 
         <Route path="/signup" element={<Signup/>} />
       </Routes>
-    
+      <ToastContainer />
+    </>
   )
 }
 
--- frontend/vite-project/src/main.jsx
+++ frontend/vite-project/src/main.jsx
@@ -4,6 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import { AuthProvider } from './context/AuthContext.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import 'react-toastify/dist/ReactToastify.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
